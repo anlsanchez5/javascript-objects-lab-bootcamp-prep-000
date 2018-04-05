e91051a1f854c929b7494f6a8e6698c044ab4c59
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-04-05 14:20:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {flour:'1 cup'}
 function updateObjectWithKeyAndValue(obj, key, value) {
- var object =  Object.assign({},obj,key,value)
+ var object =  Object.assign({},obj,{[key]:value)
   return object
   return object
 }
